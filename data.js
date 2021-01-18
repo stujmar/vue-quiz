5c7c5590663329a375c01ee85cde95253544e7fe
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-01-18 16:51:12 -0600

    wrote method to find random incorrect answers

--- data.js
+++ data.js
@@ -1,35 +1,35 @@
 data = [{
   "id": 0,
-  "question": "question",
-  "answer": "answer"  
+  "question": "question One",
+  "answer": "answer 1"  
 },
 {
     "id": 1,
-    "question": "question",
-    "answer": "answer"  
+    "question": "question Two",
+    "answer": "answer 2"  
   },
   {
     "id": 2,
-    "question": "question",
-    "answer": "answer"  
+    "question": "question Three",
+    "answer": "answer 3"  
   },
   {
     "id": 3,
-    "question": "question",
-    "answer": "answer"  
+    "question": "question Four",
+    "answer": "answer 4"  
   },
   {
     "id": 4,
-    "question": "question",
-    "answer": "answer"  
+    "question": "question Five",
+    "answer": "answer 5"  
   },
   {
     "id": 5,
-    "question": "question",
-    "answer": "answer"  
+    "question": "question Six",
+    "answer": "answer 6"  
   },
   {
     "id": 6,
-    "question": "question",
-    "answer": "answer"  
+    "question": "question Seven",
+    "answer": "answer 7"  
   }]
